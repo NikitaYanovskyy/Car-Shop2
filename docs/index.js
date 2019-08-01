0d96c1e92ca96086756754e7b8aac2ebf07f59d0
Nikita Yanovskyy <nik.yanovskyy@gmail.com>
2019-08-01 21:41:48 +0300

    i have added filter block

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,11 @@
+document.addEventListener(`DOMContentLoaded` , ()=>{
 // Mobile menu
 
-const menuButton = document.querySelector(`.burger`);
-const openMenu = document.querySelector(`.mobile-menu`);
-const closeMenu = document.querySelector(`.burger-close`);
-const main = document.querySelector(`main`);
+const 
+menuButton = document.querySelector(`.burger`),
+openMenu = document.querySelector(`.mobile-menu`),
+closeMenu = document.querySelector(`.burger-close`),
+main = document.querySelector(`main`);
 
 menuButton.addEventListener(`click` , ()=>{
     openMenu.style.display = `block`;
@@ -17,8 +19,23 @@ closeMenu.addEventListener(`click` , ()=>{
 
 
 // Search bar 
-const ulArray = document.querySelectorAll(`.search-options1 ul`);
-const optionsArray = document.querySelectorAll(`.search-border`);
+const 
+ulArray = document.querySelectorAll(`.search-options1 ul`),
+optionsArray = document.querySelectorAll(`.search-border`);
+
+// Select Buttons
+const 
+searchBTNFirst = document.querySelector(`.search-btn1`),
+searchBTNSecond = document.querySelector(`.search-btn2`);
+// Arows 
+const 
+searchArrow1 = document.querySelector(`.arrow1`);
+searchArrow2 = document.querySelector(`.arrow2`);
+// Grid blocks
+const 
+form1 = document.querySelector(`.form1`),
+form2 = document.querySelector(`.form2`);
+form2.style.display = `none`;
 
 ulArray.forEach((e , i) =>{
     optionsArray[i].addEventListener(`click` , ()=>{
@@ -29,3 +46,34 @@ ulArray.forEach((e , i) =>{
         
     })
 })
+
+searchBTNFirst.addEventListener(`click` , ()=>{
+    form2.style.display = `none`;
+    form1.style.display = `grid`;
+ 
+    searchBTNFirst.style.backgroundColor = `#fff`;
+    searchBTNFirst.style.color = `#000`;
+    searchBTNSecond.style.backgroundColor = `lightcoral`;
+    searchBTNSecond.style.color = `#fff`;
+
+    searchArrow2.style.display = `none`;
+    searchArrow1.style.display = `block`;
+
+})
+
+searchBTNSecond.addEventListener(`click` , ()=>{
+    form2.style.display = `grid`;
+    form1.style.display = `none`;
+
+    searchBTNSecond.style.backgroundColor = `#fff`;
+    searchBTNSecond.style.color = `#000`;
+    searchBTNFirst.style.backgroundColor = `lightcoral`;
+    searchBTNFirst.style.color = `#fff`;
+
+    searchArrow2.style.display = `block`;
+    searchArrow1.style.display = `none`;
+
+})
+})
+
+
